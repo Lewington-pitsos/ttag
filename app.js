7a1496fc9bad8d6aca72614292cbb90f2447a620
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-01-30 20:36:26 +1100

    express server working

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('fuckufkufkfuffk'))
+
+app.get('/poo', (req, res) => res.send('<h1>poooo</h1>'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
