f87d8fc38c9210fd0d6e11e55a7995213945fa48
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-01-30 23:09:01 +1100

    express notes added

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 
 const app = express()
 
+// this is the root directory for the purpose of requiring static files
 app.use(express.static('public'))
 
+// this sets the response for the '/' rout
+// NOTE: files served MUST have ABSOLOUTE paths (relative to this file, not the root directory defined above)
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
