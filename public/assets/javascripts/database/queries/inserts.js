98862f07b4637fc303e48a786e891682de17ba49
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-05-10 19:29:10 +1000

    insert command executing properly

--- public/assets/javascripts/database/queries/inserts.js
+++ public/assets/javascripts/database/queries/inserts.js
@@ -2,6 +2,7 @@
 const testData = {
   'Categories': [
     {
+      id: 1000,
       name: 'Books',
       intro: 'You read them...',
       image: 'books.jpg',
@@ -9,30 +10,35 @@ const testData = {
       thing_category: true
     },
     {
+      id: 1001,
       name: 'Movies',
       intro: 'You read them...',
       image: 'books.jpg',
       category_id: 1
     },
     {
+      id: 1002,
       name: 'Legal Cases',
       intro: 'You read them...',
       image: 'books.jpg',
       category_id: 1
     },
     {
+      id: 1003,
       name: 'Short Films',
       intro: 'You read them...',
       image: 'books.jpg',
       category_id: 1
     },
     {
+      id: 1004,
       name: 'Misc',
       intro: 'You read them...',
       image: 'books.jpg',
       category_id: 1
     },
     {
+      id: 1005,
       name: 'Logic Puzzles',
       intro: 'You read them...',
       image: 'books.jpg',
@@ -41,29 +47,47 @@ const testData = {
   ],
   'Things': [
     {
+      id: 1000,
       name: 'Black Adder',
       text: 'great book',
-      image: 'black_adder.jpg',
-      category_id: 2
+      image: 'black_adder.jpg'
     },
     {
+      id: 1001,
       name: 'Black Adder',
       text: 'great book',
-      image: 'black_adder.jpg',
-      category_id: 2
+      image: 'black_adder.jpg'
     },
     {
+      id: 1002,
       name: 'Black Adder',
       text: 'great book',
-      image: 'black_adder.jpg',
-      category_id: 2
+      image: 'black_adder.jpg'
     },
     {
+      id: 1003,
       name: 'Black Adder',
       text: 'great book',
-      image: 'black_adder.jpg',
-      category_id: 2
+      image: 'black_adder.jpg'
     },
+  ],
+  'ThingCategories' : [
+    {
+      category_id: 1000,
+      thing_id: 1000
+    },
+    {
+      category_id: 1000,
+      thing_id: 1001
+    },
+    {
+      category_id: 1000,
+      thing_id: 1002
+    },
+    {
+      category_id: 1000,
+      thing_id: 1003
+    }
   ]
 }
 
